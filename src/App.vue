9c519e8e7a4f4cf48ee239e89ccc41d22165cfe1
Júlio Werner <juliowernermm@gmail.com>
2022-10-24 17:47:07 -0300

    local storage

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,33 @@ export default {
 
       this.todos.push({
         title: formData.get("title"),
+        id: Math.random().toString(),
+        done: false,
       });
 
       e.target.reset();
+    },
+
+    remove(id) {
+      this.todos.splice(
+        this.todos.findIndex((todo) => todo.id === id),
+        1
+      );
+    },
+  },
+  mounted() {
+    const localTodos = localStorage.getItem("todos");
+
+    if (localTodos) {
+      this.todos = JSON.parse(localTodos);
+    }
+  },
+  watch: {
+    todos: {
+      handler(newTodos) {
+        localStorage.setItem("todos", JSON.stringify(newTodos));
+      },
+      deep: true,
     },
   },
 };
@@ -28,13 +52,38 @@ export default {
     <button>add</button>
   </form>
 
-  <ul>
-    <li
+  <div
+    :style="{
+      display: 'flex',
+      margin: '24px 0',
+      flexDirection: 'column',
+      maxWidth: '200px',
+    }"
+  >
+    <div
       v-for="todo in todos"
-      :style="{ 'text-decoration': todo.done ? 'line-through' : 'none' }"
-      @click="todo.done = !todo.done"
+      :key="todo.id"
+      :style="{
+        display: 'flex',
+        alignItems: 'center',
+      }"
     >
-      {{ todo.title }}
-    </li>
-  </ul>
+      <input type="checkbox" v-model="todo.done" />
+      <input
+        :style="{
+          textDecoration: todo.done ? 'line-through' : 'none',
+          margin: '8px 0',
+        }"
+        v-model="todo.title"
+      />
+      <button
+        :style="{
+          height: '24px',
+        }"
+        @click="remove(todo.id)"
+      >
+        x
+      </button>
+    </div>
+  </div>
 </template>
